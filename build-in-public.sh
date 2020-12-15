37f08669ad7568964bf1624b4e9250d490e25430
nasirhm <nasirhussainm14@gmail.com>
2020-12-16 03:46:36 +0500

    :wrench: Addition of a build script
    The Build script builds the project in the public/ directory which is a git submodule pointing to the main branch.
    
    Signed-off-by: nasirhm <nasirhussainm14@gmail.com>

---
+++ build-in-public.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to Github...\033[0m\n"
+
+# Build the project
+hugo -t beautifulhugo
+
+# Go to public directory
+cd public
+
+# Add changes to git
+git add .
+
+# Commit changes
+msg = "rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -S -m "$msg"
+
+# push source and build repos
+git push origin main
